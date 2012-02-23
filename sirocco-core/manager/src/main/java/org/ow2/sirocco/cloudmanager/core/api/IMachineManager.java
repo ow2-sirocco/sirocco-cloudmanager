36973d3ae3a2e5bd268a79a55c18c7b1d66c7f0e
Ruby Krishnaswamy <ruby.krishnaswamy@orange.com>
2012-02-23 15:49:34 +0000

    Add jobCompletionHandler

--- sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/api/IMachineManager.java
+++ sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/api/IMachineManager.java
@@ -112,4 +112,7 @@ public interface IMachineManager {
 		throws CloudProviderException;
 	void updateMachineTemplateCollection(Map<String, Object> attributes )
 		throws CloudProviderException;
+	
+	boolean machineCompletionHandler(final Job job);
+	
 }
