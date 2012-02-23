1eb16409032d95522cd919024c40987fdc16e6b1
Cyril Auboin <cyril.auboin@orange.com>
2012-02-23 09:21:22 +0000

    implemented deletetion

--- sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/UserManager.java
+++ sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/UserManager.java
@@ -25,6 +25,7 @@
 
 package org.ow2.sirocco.cloudmanager.core.impl;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.Resource;
@@ -40,6 +41,7 @@ import org.ow2.sirocco.cloudmanager.core.api.ICloudProviderManager;
 import org.ow2.sirocco.cloudmanager.core.api.IRemoteUserManager;
 import org.ow2.sirocco.cloudmanager.core.api.IUserManager;
 import org.ow2.sirocco.cloudmanager.core.exception.UserException;
+import org.ow2.sirocco.cloudmanager.model.cimi.CloudEntity;
 import org.ow2.sirocco.cloudmanager.model.cimi.MachineCollection;
 import org.ow2.sirocco.cloudmanager.model.cimi.MachineConfiguration;
 import org.ow2.sirocco.cloudmanager.model.cimi.MachineConfigurationCollection;
@@ -54,117 +56,119 @@ import org.ow2.sirocco.cloudmanager.model.cimi.VolumeTemplateCollection;
 @Stateless(name = IUserManager.EJB_JNDI_NAME, mappedName = IUserManager.EJB_JNDI_NAME)
 @Remote(IRemoteUserManager.class)
 @Local(IUserManager.class)
+@SuppressWarnings("unused")
 public class UserManager implements IUserManager {
 
-	private static Logger logger = Logger.getLogger(MachineImageManager.class
-			.getName());
-	@PersistenceContext(unitName = "persistence-unit/main", type = PersistenceContextType.TRANSACTION)
-	private EntityManager em;
+    private static Logger logger = Logger
+            .getLogger(UserManager.class.getName());
+    @PersistenceContext(unitName = "persistence-unit/main", type = PersistenceContextType.TRANSACTION)
+    private EntityManager em;
+    
+    @Resource
+    private SessionContext ctx;
 
-	@Resource
-	private SessionContext ctx;
+    @Override
+    public User createUser(String firstName, String lastName, String email,
+            String username, String password, String publicKey)
+            throws UserException {
+        User u = new User();
+        u.setFirstName(firstName);
+        u.setLastName(lastName);
+        u.setEmail(email);
+        u.setUsername(username);
+        u.setPassword(password);
+        u.setPublicKey(publicKey);
 
-	@Override
-	public User createUser(String firstName, String lastName, String email,
-			String username, String password, String publicKey)
-			throws UserException {
-		User u = new User();
-		u.setFirstName(firstName);
-		u.setLastName(lastName);
-		u.setEmail(email);
-		u.setUsername(username);
-		u.setPassword(password);
-		u.setPublicKey(publicKey);
+        this.em.persist(u);
 
-		this.em.persist(u);
-		
-		//create collection objects
-		MachineImageCollection mic=new MachineImageCollection();
-		mic.setUser(u);
-		MachineCollection mc=new MachineCollection();
-		mc.setUser(u);
-		VolumeCollection vc=new VolumeCollection();
-		vc.setUser(u);
-		MachineTemplateCollection mtc=new MachineTemplateCollection();
-		mtc.setUser(u);
-		MachineConfigurationCollection mcc=new MachineConfigurationCollection();
-		mcc.setUser(u);
-		VolumeTemplateCollection vtc=new VolumeTemplateCollection();
-		vtc.setUser(u);
-		VolumeConfigurationCollection vcc=new VolumeConfigurationCollection();
-		vcc.setUser(u);
-		
-		//persist them in the database
-		this.em.persist(mic);
-		this.em.persist(mc);
-		this.em.persist(vc);
-		this.em.persist(mtc);
-		this.em.persist(mcc);
-		this.em.persist(vtc);
-		this.em.persist(vcc);
-		
-		//add them to a new user 
+        // create collection objects and attach them to the user
+        MachineImageCollection mic = new MachineImageCollection();
+        mic.setUser(u);
+        MachineCollection mc = new MachineCollection();
+        mc.setUser(u);
+        VolumeCollection vc = new VolumeCollection();
+        vc.setUser(u);
+        MachineTemplateCollection mtc = new MachineTemplateCollection();
+        mtc.setUser(u);
+        MachineConfigurationCollection mcc = new MachineConfigurationCollection();
+        mcc.setUser(u);
+        VolumeTemplateCollection vtc = new VolumeTemplateCollection();
+        vtc.setUser(u);
+        VolumeConfigurationCollection vcc = new VolumeConfigurationCollection();
+        vcc.setUser(u);
 
-		
-		
-		
-		
-		
-		
-		
-		return u;
-	}
+        // persist them in the database
+        this.em.persist(mic);
+        this.em.persist(mc);
+        this.em.persist(vc);
+        this.em.persist(mtc);
+        this.em.persist(mcc);
+        this.em.persist(vtc);
+        this.em.persist(vcc);
 
-	@Override
-	public User getUserById(String userId) throws UserException {
+        return u;
+    }
 
-		User result = this.em.find(User.class, new Integer(userId));
+    @Override
+    public User getUserById(String userId) throws UserException {
 
-		return result;
-	}
+        User result = this.em.find(User.class, new Integer(userId));
 
-	@Override
-	public User getUserByUsername(String userName) throws UserException {
+        return result;
+    }
 
-		User u = null;
-		try {
-			u = (User) this.em
-					.createQuery("FROM User u WHERE u.username=:usrname")
-					.setParameter("usrname", userName).getResultList().get(0);
-		} catch (IndexOutOfBoundsException e) {
-			throw new UserException();
-		}
+    @SuppressWarnings("unchecked")
+    @Override
+    public User getUserByUsername(String userName) throws UserException {
 
-		return u;
-	}
+        User u = null;
 
-	@Override
-	public User updateUser(User user)
-			throws UserException {
-		/*User u=new User(); //= getUserById(userId);
-		if (firstName!=null){u.setFirstName(firstName);}
-		if (lastName!=null){u.setLastName(lastName);}
-		if (email!=null){u.setEmail(email);}
-		if (username!=null){u.setUsername(username);}
-		if (password!=null){u.setPassword(password);}
-		if (publicKey!=null){u.setPublicKey(publicKey);}*/
-		
-		this.em.merge(user);
-		
-		return user;
-	}
+        List<User> l = this.em
+                .createQuery("FROM User u WHERE u.username=:usrname")
+                .setParameter("usrname", userName).getResultList();
 
-	@Override
-	public void deleteUser(String userId) throws UserException {
+        for (User lu : l) {
+            u = lu;
+            break;
+        }
+        return u;
+    }
 
-		User result = this.getUserById(userId);
-		
-		//List l<List>=this.em
-		//.createQuery("FROM User u WHERE u.id=:usrid")
-		//.setParameter("usrid", userId).getResultList();
-		
-		this.em.remove(result);
+    @Override
+    public User updateUser(User user) throws UserException {
 
-	}
+        Integer userId = user.getId();
+        this.em.merge(user);
+
+        return this.getUserById(userId.toString());
+    }
+
+    @Override
+    public void deleteUser(String userId) throws UserException {
+
+        User result = this.getUserById(userId);
+
+        removeCollection("MachineImageCollection", result);
+        removeCollection("MachineCollection", result);
+        removeCollection("VolumeCollection", result);
+        removeCollection("MachineTemplateCollection", result);
+        removeCollection("MachineConfigurationCollection", result);
+        removeCollection("VolumeTemplateCollection", result);
+        removeCollection("VolumeConfigurationCollection", result);
+
+        this.em.remove(result);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private void removeCollection(String Type, User u) {
+        List<MachineImageCollection> l = this.em
+                .createQuery("FROM " + Type + " t WHERE t.user.id=:usrid")
+                .setParameter("usrid", u.getId()).getResultList();
+        for (CloudEntity lmic : l) {
+            lmic.setUser(null);
+            this.em.remove(lmic);
+        }
+    }
 
 }
