ff4669211f559c863f2e65b3a56480eea8be05d5
Frederic Dang Tran <frederic.dangtran@orange.com>
2012-02-24 13:08:08 +0000

    Removing obsolete maven modules

--- sirocco-ejb-jar/src/main/java/org/ow2/sirocco/cloudmanager/service/api/ITimer.java
+++
@@ -1,7 +0,0 @@
-package org.ow2.sirocco.cloudmanager.service.api;
-
-public interface ITimer {
-    void startTimer();
-
-    void stopTimer();
-}
--- sirocco-providers/util/vncproxy/html2java.sh
+++
@@ -1,1 +0,0 @@
-sed -e 's/"/\\"/g' src/main/resources/vnc_auto.html | while read -r line; do echo out.println\(\"$line\"\)\;; done
