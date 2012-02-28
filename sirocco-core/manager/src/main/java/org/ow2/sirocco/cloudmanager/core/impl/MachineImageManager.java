b03c18a08906c3abdce64acf464a5bd34a396db0
Frederic Dang Tran <frederic.dangtran@orange.com>
2012-02-28 16:55:43 +0000

    Updated code formatting

--- sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/MachineImageManager.java
+++ sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/MachineImageManager.java
@@ -25,109 +25,104 @@
 
 package org.ow2.sirocco.cloudmanager.core.impl;
 
-
-import java.util.List;
 import java.util.Date;
-import java.util.logging.Level;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Remote;
-import javax.ejb.Stateless;
 import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
-import javax.persistence.Query;
 
-import org.ow2.sirocco.cloudmanager.model.cimi.MachineImage;
-import org.ow2.sirocco.cloudmanager.model.cimi.Job;
-import org.ow2.sirocco.cloudmanager.model.cimi.User;
-import org.ow2.sirocco.cloudmanager.core.api.IUserManager;
 import org.ow2.sirocco.cloudmanager.core.api.IMachineImageManager;
 import org.ow2.sirocco.cloudmanager.core.api.IRemoteMachineImageManager;
-
+import org.ow2.sirocco.cloudmanager.core.api.IUserManager;
 import org.ow2.sirocco.cloudmanager.core.exception.CloudProviderException;
 import org.ow2.sirocco.cloudmanager.core.exception.UserException;
-
+import org.ow2.sirocco.cloudmanager.model.cimi.Job;
+import org.ow2.sirocco.cloudmanager.model.cimi.MachineImage;
+import org.ow2.sirocco.cloudmanager.model.cimi.User;
 
 @Stateless(name = IMachineImageManager.EJB_JNDI_NAME, mappedName = IMachineImageManager.EJB_JNDI_NAME)
 @Remote(IRemoteMachineImageManager.class)
 @Local(IMachineImageManager.class)
 public class MachineImageManager implements IMachineImageManager {
 
-	private static Logger logger = Logger.getLogger(MachineImageManager.class.getName());
+    private static Logger logger = Logger.getLogger(MachineImageManager.class.getName());
 
-	@PersistenceContext(unitName = "persistence-unit/main", type = PersistenceContextType.TRANSACTION)
-	private EntityManager em;
+    @PersistenceContext(unitName = "persistence-unit/main", type = PersistenceContextType.TRANSACTION)
+    private EntityManager em;
 
-	@EJB
+    @EJB
     private IUserManager userManager;
-	
-	@Resource
-	private SessionContext		ctx;
-	private	User				user;
 
-	@Resource
-	public void setSessionContext(SessionContext ctx) {
-		this.ctx = ctx;
-	}
+    @Resource
+    private SessionContext ctx;
 
-	private void setUser() throws UserException {
-		String username = ctx.getCallerPrincipal().getName();
-		user = userManager.getUserByUsername(username);
-	}
+    private User user;
 
-	public Job createMachineImage(MachineImage mi) throws CloudProviderException  {
-		Job j = new Job();
-	
-		mi.setUser(user);
-		mi.setCreated(new Date());
-		mi.setState(MachineImage.State.AVAILABLE);
-		this.em.persist(mi);
-		this.em.flush();
-		
-		j.setTargetEntity(mi.getId().toString());
-		j.setStatus(Job.Status.SUCCESS);
-		j.setAction("create");
-		j.setParentJob(null);
-		j.setNestedJobs(null);
-		j.setReturnCode(0);
-		j.setUser(user);
-		this.em.persist(j);
-		this.em.flush();
-		return j;
-	}
+    @Resource
+    public void setSessionContext(final SessionContext ctx) {
+        this.ctx = ctx;
+    }
 
-	public List<MachineImage> getMachineImages() throws CloudProviderException {
-		setUser();
-		List<MachineImage> images = null;
-		try {
-			images = this.em.createQuery("FROM MachineImage i WHERE i.state<>'DELETED' AND i.user=:user").setParameter("user", user).getResultList();
-		} catch (Exception e) {
-			throw new CloudProviderException("Internal query error");
-		}
-		return images;
-	}
+    private void setUser() throws UserException {
+        String username = this.ctx.getCallerPrincipal().getName();
+        this.user = this.userManager.getUserByUsername(username);
+    }
 
-	public MachineImage getMachineImage(String imageId)
-			throws CloudProviderException {
+    public Job createMachineImage(final MachineImage mi) throws CloudProviderException {
+        Job j = new Job();
 
-		MachineImage image = null;
-		try {
-			image = this.em.find(MachineImage.class, Integer.valueOf(new String(imageId)));
+        mi.setUser(this.user);
+        mi.setCreated(new Date());
+        mi.setState(MachineImage.State.AVAILABLE);
+        this.em.persist(mi);
+        this.em.flush();
 
-		} catch (Exception e) {
-			throw new CloudProviderException("MachineImage of identity " +imageId + " cannot be found ");
-		}
-		return image;
-	}
+        j.setTargetEntity(mi.getId().toString());
+        j.setStatus(Job.Status.SUCCESS);
+        j.setAction("create");
+        j.setParentJob(null);
+        j.setNestedJobs(null);
+        j.setReturnCode(0);
+        j.setUser(this.user);
+        this.em.persist(j);
+        this.em.flush();
+        return j;
+    }
 
-	public void deleteMachineImage(String imageId)throws CloudProviderException {
+    public List<MachineImage> getMachineImages() throws CloudProviderException {
+        this.setUser();
+        List<MachineImage> images = null;
+        try {
+            images = this.em.createQuery("FROM MachineImage i WHERE i.state<>'DELETED' AND i.user=:user")
+                .setParameter("user", this.user).getResultList();
+        } catch (Exception e) {
+            throw new CloudProviderException("Internal query error");
+        }
+        return images;
+    }
 
-	}
+    public MachineImage getMachineImage(final String imageId) throws CloudProviderException {
+
+        MachineImage image = null;
+        try {
+            image = this.em.find(MachineImage.class, Integer.valueOf(new String(imageId)));
+
+        } catch (Exception e) {
+            throw new CloudProviderException("MachineImage of identity " + imageId + " cannot be found ");
+        }
+        return image;
+    }
+
+    public void deleteMachineImage(final String imageId) throws CloudProviderException {
+
+    }
 
 }
