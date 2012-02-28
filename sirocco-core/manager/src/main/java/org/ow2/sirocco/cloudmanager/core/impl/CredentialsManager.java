5f35979a52eaa916cb669487100fbf1a70e53ca8
Frederic Dang Tran <frederic.dangtran@orange.com>
2012-02-28 16:54:57 +0000

    Fixed called to Credentials.setPublicKey

--- sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/CredentialsManager.java
+++ sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/CredentialsManager.java
@@ -27,9 +27,8 @@ package org.ow2.sirocco.cloudmanager.core.impl;
 
 import java.util.Date;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
 import javax.ejb.EJB;
@@ -41,121 +40,128 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 
-import org.ow2.sirocco.cloudmanager.model.cimi.Credentials;
-import org.ow2.sirocco.cloudmanager.model.cimi.User;
-import org.ow2.sirocco.cloudmanager.model.cimi.MachineTemplate;
 import org.ow2.sirocco.cloudmanager.core.api.ICredentialsManager;
 import org.ow2.sirocco.cloudmanager.core.api.IRemoteCredentialsManager;
 import org.ow2.sirocco.cloudmanager.core.api.IUserManager;
 import org.ow2.sirocco.cloudmanager.core.exception.CloudProviderException;
 import org.ow2.sirocco.cloudmanager.core.exception.InvalidRequestException;
 import org.ow2.sirocco.cloudmanager.core.exception.ResourceNotFoundException;
-
+import org.ow2.sirocco.cloudmanager.model.cimi.Credentials;
+import org.ow2.sirocco.cloudmanager.model.cimi.MachineTemplate;
+import org.ow2.sirocco.cloudmanager.model.cimi.User;
 
 @Stateless(name = ICredentialsManager.EJB_JNDI_NAME, mappedName = ICredentialsManager.EJB_JNDI_NAME)
 @Remote(IRemoteCredentialsManager.class)
 @Local(ICredentialsManager.class)
 public class CredentialsManager implements ICredentialsManager {
 
-	private static Logger logger = Logger.getLogger(CredentialsManager.class.getName());
-	@PersistenceContext(unitName = "persistence-unit/main", type = PersistenceContextType.TRANSACTION)
-	private EntityManager em;
-	
-	@Resource
-	private SessionContext		ctx;
-	
-	@EJB
-    private IUserManager userManager;
-	private	User				user;
-	
-	@Resource
-	public void setSessionContext(SessionContext ctx) {
-		this.ctx = ctx;
-	}
+    private static Logger logger = Logger.getLogger(CredentialsManager.class.getName());
 
-	private void setUser() throws CloudProviderException {
-		String username = ctx.getCallerPrincipal().getName();
-		user = userManager.getUserByUsername(username);
-	}	
-	
-	private void validateCredentials(Credentials cred) throws CloudProviderException {
-		if (cred.getUserName().length() < 1) {
-			throw new InvalidRequestException("Too short user name ");
-		}
-		if (Pattern.matches("[\\w]*$", cred.getUserName()) == false) {
-			throw new InvalidRequestException("Non word characters in user name ");
-		}
-		if (cred.getPassword().length() < 1) {
-			throw new InvalidRequestException("Too short password ");
-		}
-		if (Pattern.matches("[\\w]*$", cred.getPassword()) == false) {
-			throw new InvalidRequestException("Non word characters in user name ");
-		}
-		if (Pattern.matches("[^\\s]*$", cred.getPassword()) == false) {
-			throw new InvalidRequestException("Spaces in password ");
-		}
-	}
-	public Credentials createCredentials(Credentials credentials) throws CloudProviderException {
-		setUser();
-		// validate user
-		validateCredentials(credentials);
-		credentials.setCreated(new Date());
-		credentials.setUser(user);
-		this.em.persist(credentials);
-		return credentials;
-	}
-	
-    public Credentials updateCredentials(Credentials credentials) throws CloudProviderException {
-    	throw new InvalidRequestException(" Cannot update credentials ");
+    @PersistenceContext(unitName = "persistence-unit/main", type = PersistenceContextType.TRANSACTION)
+    private EntityManager em;
+
+    @Resource
+    private SessionContext ctx;
+
+    @EJB
+    private IUserManager userManager;
+
+    private User user;
+
+    @Resource
+    public void setSessionContext(final SessionContext ctx) {
+        this.ctx = ctx;
     }
-    
-    public Credentials getCredentialsById(String credentialsId) throws CloudProviderException {
-    	setUser();
-    	if (credentialsId == null) {
-    		throw new InvalidRequestException("null credentials id");
-    	}
-    	Credentials cred = this.em.find(Credentials.class, Integer.valueOf(credentialsId));
-    	if (cred == null) {
-    		throw new ResourceNotFoundException("Credentials " +credentialsId +" not found");
-    	}
-    	/** Would be nice to use em.detach instead
-    	 */
-    	Credentials c = new Credentials();
-    	c.setUserName(cred.getUserName());
-    	c.setName(cred.getName());
-    	c.setId(cred.getId());
-    	c.setProperties(cred.getProperties());
-    	c.setCreated(cred.getCreated());
-    	c.setUpdated(cred.getUpdated());
-    	c.setPassword("");
-    	c.setKey(null);
-    	return cred;
+
+    private void setUser() throws CloudProviderException {
+        String username = this.ctx.getCallerPrincipal().getName();
+        this.user = this.userManager.getUserByUsername(username);
     }
-    
-    
-    public void deleteCredentials(String credentialsId) throws CloudProviderException {
-    	setUser();
-    	if (credentialsId == null) {
-    		throw new InvalidRequestException("null credentials id");
-    	}
-    	Credentials cred = this.em.find(Credentials.class, Integer.valueOf(credentialsId));
-    	/** if anymachine template refers to this credential
-    	 * do not delete.
-    	 */
-    	List<MachineTemplate> mts = null;
-    	try {
-    		mts = this.em.createQuery("FROM MachineTemplate t WHERE t.credentials=:cred").setParameter("cred", cred).getResultList();
-    	} catch (Exception e) {
-    		throw new CloudProviderException(" Internal Error");
-    	}
-    	if (mts.size() != 0) {
-    		throw new InvalidRequestException(" Credential " +credentialsId +" is in use");
-    	}
-    	
-    	/** delete credentials */
-    	this.em.remove(cred);
-    	this.em.flush();
-    	return;
+
+    private void validateCredentials(final Credentials cred) throws CloudProviderException {
+        if (cred.getUserName().length() < 1) {
+            throw new InvalidRequestException("Too short user name ");
+        }
+        if (Pattern.matches("[\\w]*$", cred.getUserName()) == false) {
+            throw new InvalidRequestException("Non word characters in user name ");
+        }
+        if (cred.getPassword().length() < 1) {
+            throw new InvalidRequestException("Too short password ");
+        }
+        if (Pattern.matches("[\\w]*$", cred.getPassword()) == false) {
+            throw new InvalidRequestException("Non word characters in user name ");
+        }
+        if (Pattern.matches("[^\\s]*$", cred.getPassword()) == false) {
+            throw new InvalidRequestException("Spaces in password ");
+        }
+    }
+
+    public Credentials createCredentials(final Credentials credentials) throws CloudProviderException {
+        this.setUser();
+        // validate user
+
+        this.validateCredentials(credentials);
+        credentials.setCreated(new Date());
+        credentials.setUser(this.user);
+        CredentialsManager.logger.info("Persist credentials  " + credentials.getName());
+        this.em.persist(credentials);
+        this.em.flush();
+        CredentialsManager.logger.info("Persist credentials return " + credentials.getId());
+        return credentials;
+    }
+
+    public Credentials updateCredentials(final Credentials credentials) throws CloudProviderException {
+        throw new InvalidRequestException(" Cannot update credentials ");
+    }
+
+    public Credentials getCredentialsById(final String credentialsId) throws CloudProviderException {
+        this.setUser();
+        if (credentialsId == null) {
+            throw new InvalidRequestException("null credentials id");
+        }
+        Credentials cred = this.em.find(Credentials.class, Integer.valueOf(credentialsId));
+        if (cred == null) {
+            throw new ResourceNotFoundException("Credentials " + credentialsId + " not found");
+        }
+        /**
+         * Would be nice to use em.detach instead
+         */
+        Credentials c = new Credentials();
+        c.setUserName(cred.getUserName());
+        c.setName(cred.getName());
+        c.setId(cred.getId());
+        c.setProperties(cred.getProperties());
+        c.setCreated(cred.getCreated());
+        c.setUpdated(cred.getUpdated());
+        c.setPassword("");
+        c.setPublicKey(null);
+        return cred;
+    }
+
+    public void deleteCredentials(final String credentialsId) throws CloudProviderException {
+        this.setUser();
+        if (credentialsId == null) {
+            throw new InvalidRequestException("null credentials id");
+        }
+        Credentials cred = this.em.find(Credentials.class, Integer.valueOf(credentialsId));
+        /**
+         * if anymachine template refers to this credential do not delete.
+         */
+        List<MachineTemplate> mts = null;
+        try {
+            mts = this.em.createQuery("FROM MachineTemplate t WHERE t.credentials=:cred").setParameter("cred", cred)
+                .getResultList();
+        } catch (Exception e) {
+            throw new CloudProviderException(" Internal Error");
+        }
+        if (mts.size() != 0) {
+            throw new InvalidRequestException(" Credential " + credentialsId + " is in use");
+        }
+
+        /** delete credentials */
+        this.em.remove(cred);
+        this.em.flush();
+        return;
     }
 
 }
