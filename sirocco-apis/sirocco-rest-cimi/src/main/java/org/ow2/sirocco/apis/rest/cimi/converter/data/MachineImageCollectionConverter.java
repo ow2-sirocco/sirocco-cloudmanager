ef4216b5138e323f887e9b604ff1857e4bfb58c1
Michel-Ange Anton <michel-ange.anton@bull.net>
2012-03-09 08:15:39 +0000

    Add svn:keywords

--- sirocco-apis/sirocco-rest-cimi/src/main/java/org/ow2/sirocco/apis/rest/cimi/converter/data/MachineImageCollectionConverter.java
+++ sirocco-apis/sirocco-rest-cimi/src/main/java/org/ow2/sirocco/apis/rest/cimi/converter/data/MachineImageCollectionConverter.java
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  *
- * $Id: MachineImageConverter.java 1091 2012-03-08 17:21:58Z antonma $
+ * $Id$
  *
  */
 package org.ow2.sirocco.apis.rest.cimi.converter.data;
