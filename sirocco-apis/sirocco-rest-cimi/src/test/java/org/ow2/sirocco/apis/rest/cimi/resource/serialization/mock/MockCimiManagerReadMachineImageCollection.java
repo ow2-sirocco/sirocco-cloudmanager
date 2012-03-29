5bcb4a2a09824cb02d24c2cab106dfeeca7d052e
Michel-Ange Anton <michel-ange.anton@bull.net>
2012-03-29 13:24:32 +0000

    Serialization tests : - GET MachineImage and MachineImageCollection - correction of the serialization of CimiCommon and CimiMachineImageCollection

---
+++ sirocco-apis/sirocco-rest-cimi/src/test/java/org/ow2/sirocco/apis/rest/cimi/resource/serialization/mock/MockCimiManagerReadMachineImageCollection.java
@@ -0,0 +1,56 @@
+/**
+ *
+ * SIROCCO
+ * Copyright (C) 2011 France Telecom
+ * Contact: sirocco@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ *
+ * $Id$
+ *
+ */
+package org.ow2.sirocco.apis.rest.cimi.resource.serialization.mock;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.ow2.sirocco.apis.rest.cimi.builder.CimiEntityBuilderHelper;
+import org.ow2.sirocco.apis.rest.cimi.manager.CimiManager;
+import org.ow2.sirocco.apis.rest.cimi.request.CimiRequest;
+import org.ow2.sirocco.apis.rest.cimi.request.CimiResponse;
+
+/**
+ * Mock CimiManagerReadMachineImageCollection.
+ */
+public class MockCimiManagerReadMachineImageCollection implements CimiManager {
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.ow2.sirocco.apis.rest.cimi.manager.CimiManager#execute(org.ow2.sirocco.apis.rest.cimi.request.CimiRequest,
+     *      org.ow2.sirocco.apis.rest.cimi.request.CimiResponse)
+     */
+    @Override
+    public void execute(final CimiRequest request, final CimiResponse response) {
+        try {
+            response.setCimiData(CimiEntityBuilderHelper.buildCimiMachineImageCollection(request.getHeader()
+                .getIntegerSiroccoInfoTestId(), request.getHeader().getBooleanSiroccoInfoTestExpand()));
+            response.setStatus(Status.OK);
+        } catch (Exception e) {
+            response.setStatus(Status.BAD_REQUEST);
+        }
+    }
+
+}
