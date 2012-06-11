602afdbbb36838526ff1028ce484cfe89b0bf308
Cyril Auboin <cyril.auboin@orange.com>
2012-06-11 15:37:43 +0000

    added DELETED to cimi entity system

--- sirocco-model/cimi/src/main/java/org/ow2/sirocco/cloudmanager/model/cimi/System.java
+++ sirocco-model/cimi/src/main/java/org/ow2/sirocco/cloudmanager/model/cimi/System.java
@@ -46,7 +46,7 @@ public class System extends CloudResource implements Serializable {
     private CloudProviderLocation location;
 
     public static enum State {
-        CREATING, CREATED, STARTING, STARTED, STOPPING, STOPPED, PAUSING, PAUSED, SUSPENDING, SUSPENDED, MIXED, DELETING, ERROR
+        CREATING, CREATED, STARTING, STARTED, STOPPING, STOPPED, PAUSING, PAUSED, SUSPENDING, SUSPENDED, MIXED, DELETING, DELETED, ERROR
     }
 
     private List<Credentials> credentials;
