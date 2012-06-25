5004d3817b9a42293f8f8440d1f3a04d9f9e7b61
Ruby Krishnaswamy <ruby.krishnaswamy@orange.com>
2012-06-25 15:46:06 +0000

    Missing EventLogCreate

---
+++ sirocco-model/cimi/src/main/java/org/ow2/sirocco/cloudmanager/model/cimi/event/EventLogCreate.java
@@ -0,0 +1,74 @@
+/**
+ *
+ * SIROCCO
+ * Copyright (C) 2011 France Telecom
+ * Contact: sirocco@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  $Id$
+ *
+ */
+
+package org.ow2.sirocco.cloudmanager.model.cimi.event;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class EventLogCreate implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    private String description;
+
+    private Map<String, String> properties;
+
+    private EventLogTemplate eventLogTemplate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map<String, String> properties) {
+        this.properties = properties;
+    }
+
+    public EventLogTemplate getEventLogTemplate() {
+        return eventLogTemplate;
+    }
+
+    public void setEventLogTemplate(final EventLogTemplate eventLogTemplate) {
+        this.eventLogTemplate = eventLogTemplate;
+    }
+    
+}
