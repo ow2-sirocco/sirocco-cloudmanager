3d51c3bde2b5f242f77f2d51162fdef9cba86496
Michel-Ange Anton <michel-ange.anton@bull.net>
2012-06-26 15:48:24 +0000

    Extract ID Volume of imageLocation and verify it before using the service to create with snapshot

--- sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-apis/src/main/java/org/ow2/sirocco/apis/rest/cimi/manager/volume/image/CimiManagerCreateVolumeImage.java
+++ sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-apis/src/main/java/org/ow2/sirocco/apis/rest/cimi/manager/volume/image/CimiManagerCreateVolumeImage.java
@@ -24,11 +24,14 @@
  */
 package org.ow2.sirocco.apis.rest.cimi.manager.volume.image;
 
+import org.ow2.sirocco.apis.rest.cimi.converter.HrefHelper;
+import org.ow2.sirocco.apis.rest.cimi.domain.CimiVolume;
 import org.ow2.sirocco.apis.rest.cimi.domain.CimiVolumeImage;
 import org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerCreateAbstract;
 import org.ow2.sirocco.apis.rest.cimi.manager.MergeReferenceHelper;
 import org.ow2.sirocco.apis.rest.cimi.request.CimiContext;
 import org.ow2.sirocco.cloudmanager.core.api.IVolumeManager;
+import org.ow2.sirocco.cloudmanager.model.cimi.Volume;
 import org.ow2.sirocco.cloudmanager.model.cimi.VolumeImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -56,7 +59,25 @@ public class CimiManagerCreateVolumeImage extends CimiManagerCreateAbstract {
      */
     @Override
     protected Object callService(final CimiContext context, final Object dataService) throws Exception {
-        return this.manager.createVolumeImage((VolumeImage) dataService);
+        Object out = null;
+        String idVolumeSnapshot = null;
+        VolumeImage image = (VolumeImage) dataService;
+        // Extract ID Volume of imageLocation and verify it before using the
+        // service to create with snapshot
+        if (null != image.getImageLocation()) {
+            idVolumeSnapshot = HrefHelper.extractIdString(image.getImageLocation());
+            if (false == image.getImageLocation().equals(context.makeHref(CimiVolume.class, idVolumeSnapshot))) {
+                idVolumeSnapshot = null;
+            }
+        }
+        // Call services
+        if (null != idVolumeSnapshot) {
+            Volume volume = this.manager.getVolumeById(idVolumeSnapshot);
+            out = this.manager.createVolumeSnapshot(volume, (VolumeImage) dataService);
+        } else {
+            out = this.manager.createVolumeImage((VolumeImage) dataService);
+        }
+        return out;
     }
 
     /**
