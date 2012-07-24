b8d577cfac845f1df21d766597c2b5eb7f9b3036
Frederic Dang Tran <frederic.dangtran@orange.com>
2012-07-24 08:13:13 +0000

    Added dump of exception trace

--- sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/JobCompletionHandlerBean.java
+++ sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/JobCompletionHandlerBean.java
@@ -26,6 +26,7 @@
 package org.ow2.sirocco.cloudmanager.core.impl;
 
 import java.io.Serializable;
+
 import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.EJB;
@@ -42,16 +43,16 @@ import javax.jms.TopicConnectionFactory;
 import javax.jms.TopicPublisher;
 import javax.jms.TopicSession;
 import javax.naming.InitialContext;
+
 import org.apache.log4j.Logger;
 import org.ow2.sirocco.cloudmanager.core.api.IJobManager;
 import org.ow2.sirocco.cloudmanager.model.cimi.Job;
 
 @MessageDriven(activationConfig = {
-        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
-        @ActivationConfigProperty(propertyName = "destination", propertyValue = "JobCompletion") })
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+    @ActivationConfigProperty(propertyName = "destination", propertyValue = "JobCompletion")})
 public class JobCompletionHandlerBean implements MessageListener {
-    private static Logger logger = Logger
-            .getLogger(JobCompletionHandlerBean.class.getName());
+    private static Logger logger = Logger.getLogger(JobCompletionHandlerBean.class.getName());
 
     private static final String JMS_TOPIC_CONNECTION_FACTORY_NAME = "JTCF";
 
@@ -66,32 +67,30 @@ public class JobCompletionHandlerBean implements MessageListener {
     private EJBContext ctx;
 
     @Override
-    //@dTransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    // @dTransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public void onMessage(final Message msg) {
         if (msg instanceof ObjectMessage) {
             ObjectMessage objectMessage = (ObjectMessage) msg;
             Object payload;
             try {
                 payload = objectMessage.getObject();
-                JobCompletionHandlerBean.logger
-                        .info("On topic JobCompletion: received " + payload);
+                JobCompletionHandlerBean.logger.info("On topic JobCompletion: received " + payload);
             } catch (JMSException ex) {
-                JobCompletionHandlerBean.logger.error(
-                        "Failed to extract from JMS message", ex);
+                JobCompletionHandlerBean.logger.error("Failed to extract from JMS message", ex);
                 return;
             }
             Job providerJob = (Job) payload;
             // we call jobManager to deal with events
             try {
-                jobManager.handleWorkflowEvent(providerJob);
+                this.jobManager.handleWorkflowEvent(providerJob);
             } catch (Exception e) {
-                ctx.setRollbackOnly();
-                JobCompletionHandlerBean.logger
-                        .warn("JobCompletion message rollbacked - "+e.getMessage());
+                this.ctx.setRollbackOnly();
+                JobCompletionHandlerBean.logger.warn("JobCompletion message rollbacked - " + e.getMessage(), e);
 
                 try {
                     // not possible to set a redelevery time in Joram/Jonas
-                    Thread.sleep(JMS_REDELIVERY_DELAY+(long)Math.floor(Math.random()*JMS_REDELIVERY_DELAY));
+                    Thread.sleep(JobCompletionHandlerBean.JMS_REDELIVERY_DELAY
+                        + (long) Math.floor(Math.random() * JobCompletionHandlerBean.JMS_REDELIVERY_DELAY));
                 } catch (InterruptedException e1) {
                     e1.printStackTrace();
                 }
@@ -103,21 +102,16 @@ public class JobCompletionHandlerBean implements MessageListener {
     private void emitMessage(final Serializable payload) throws Exception {
         InitialContext ctx = new InitialContext();
         TopicConnectionFactory topicConnectionFactory = (TopicConnectionFactory) ctx
-                .lookup(JobCompletionHandlerBean.JMS_TOPIC_CONNECTION_FACTORY_NAME);
-        TopicConnection connection = topicConnectionFactory
-                .createTopicConnection();
-        TopicSession session = connection.createTopicSession(false,
-                Session.AUTO_ACKNOWLEDGE);
-        Topic cloudAdminTopic = (Topic) ctx
-                .lookup(JobCompletionHandlerBean.JMS_TOPIC_NAME);
-        TopicPublisher topicPublisher = session
-                .createPublisher(cloudAdminTopic);
+            .lookup(JobCompletionHandlerBean.JMS_TOPIC_CONNECTION_FACTORY_NAME);
+        TopicConnection connection = topicConnectionFactory.createTopicConnection();
+        TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic cloudAdminTopic = (Topic) ctx.lookup(JobCompletionHandlerBean.JMS_TOPIC_NAME);
+        TopicPublisher topicPublisher = session.createPublisher(cloudAdminTopic);
         ObjectMessage message = session.createObjectMessage();
         message.setObject(payload);
         topicPublisher.publish(message);
-        JobCompletionHandlerBean.logger.info("EMITTED EVENT "
-                + payload.toString() + " on "
-                + JobCompletionHandlerBean.JMS_TOPIC_NAME + " topic");
+        JobCompletionHandlerBean.logger.info("EMITTED EVENT " + payload.toString() + " on "
+            + JobCompletionHandlerBean.JMS_TOPIC_NAME + " topic");
         topicPublisher.close();
         session.close();
         connection.close();
