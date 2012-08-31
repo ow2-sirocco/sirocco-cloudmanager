be22da24dc5ba67553b4fa17579b65a4d3209371
Michel-Ange Anton <michel-ange.anton@bull.net>
2012-08-31 10:11:05 +0000

    Added REST managers : Network, NetworkPort, Address, ForwardingGroup

---
+++ sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-apis/src/main/java/org/ow2/sirocco/apis/rest/cimi/manager/network/template/CimiManagerCreateNetworkTemplate.java
@@ -0,0 +1,88 @@
+/**
+ *
+ * SIROCCO
+ * Copyright (C) 2011 France Telecom
+ * Contact: sirocco@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ *
+ * $Id$
+ *
+ */
+package org.ow2.sirocco.apis.rest.cimi.manager.network.template;
+
+import javax.ws.rs.core.Response;
+
+import org.ow2.sirocco.apis.rest.cimi.domain.CimiNetworkTemplate;
+import org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerCreateAbstract;
+import org.ow2.sirocco.apis.rest.cimi.manager.MergeReferenceHelper;
+import org.ow2.sirocco.apis.rest.cimi.request.CimiContext;
+import org.ow2.sirocco.apis.rest.cimi.utils.Constants;
+import org.ow2.sirocco.cloudmanager.core.api.INetworkManager;
+import org.ow2.sirocco.cloudmanager.model.cimi.NetworkTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * Manage CREATE request of NetworkTemplate.
+ */
+@Component("CimiManagerCreateNetworkTemplate")
+public class CimiManagerCreateNetworkTemplate extends CimiManagerCreateAbstract {
+
+    @Autowired
+    @Qualifier("INetworkManager")
+    private INetworkManager manager;
+
+    @Autowired
+    @Qualifier("MergeReferenceHelper")
+    private MergeReferenceHelper mergeReference;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerAbstract#callService(org.ow2.sirocco.apis.rest.cimi.request.CimiContext,
+     *      java.lang.Object)
+     */
+    @Override
+    protected Object callService(final CimiContext context, final Object dataService) throws Exception {
+        return this.manager.createNetworkTemplate((NetworkTemplate) dataService);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerAbstract#convertToResponse(org.ow2.sirocco.apis.rest.cimi.request.CimiContext,
+     *      java.lang.Object)
+     */
+    @Override
+    protected void convertToResponse(final CimiContext context, final Object dataService) throws Exception {
+        CimiNetworkTemplate cimi = (CimiNetworkTemplate) context.convertToCimi(dataService, CimiNetworkTemplate.class);
+        context.getResponse().setCimiData(cimi);
+        context.getResponse().putHeader(Constants.HEADER_LOCATION, cimi.getId());
+        context.getResponse().setStatus(Response.Status.CREATED);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerAbstract#beforeConvertToDataService(org.ow2.sirocco.apis.rest.cimi.request.CimiContext)
+     */
+    @Override
+    protected void beforeConvertToDataService(final CimiContext context) throws Exception {
+        this.mergeReference.merge(context, (CimiNetworkTemplate) context.getRequest().getCimiData());
+    }
+}
---
+++ sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-apis/src/main/java/org/ow2/sirocco/apis/rest/cimi/manager/networkport/template/CimiManagerReadNetworkPortTemplateCollection.java
@@ -0,0 +1,96 @@
+/**
+ *
+ * SIROCCO
+ * Copyright (C) 2011 France Telecom
+ * Contact: sirocco@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ *
+ * $Id$
+ *
+ */
+package org.ow2.sirocco.apis.rest.cimi.manager.networkport.template;
+
+import javax.ws.rs.core.Response;
+
+import org.ow2.sirocco.apis.rest.cimi.domain.CimiOperation;
+import org.ow2.sirocco.apis.rest.cimi.domain.CimiResource;
+import org.ow2.sirocco.apis.rest.cimi.domain.Operation;
+import org.ow2.sirocco.apis.rest.cimi.domain.collection.CimiNetworkPortTemplateCollectionRoot;
+import org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerReadAbstract;
+import org.ow2.sirocco.apis.rest.cimi.request.CimiContext;
+import org.ow2.sirocco.cloudmanager.core.api.INetworkManager;
+import org.ow2.sirocco.cloudmanager.core.api.QueryResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * Manage READ request of NetworkPortTemplates collection.
+ */
+@Component("CimiManagerReadNetworkPortTemplateCollection")
+public class CimiManagerReadNetworkPortTemplateCollection extends CimiManagerReadAbstract {
+
+    @Autowired
+    @Qualifier("INetworkManager")
+    private INetworkManager manager;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerAbstract#callService(org.ow2.sirocco.apis.rest.cimi.request.CimiContext,
+     *      java.lang.Object)
+     */
+    @Override
+    protected Object callService(final CimiContext context, final Object dataService) throws Exception {
+        Object out = null;
+        if (false == context.hasParamsForReadingCollection()) {
+            out = this.manager.getNetworkPortTemplates();
+        } else {
+            QueryResult<?> results = this.manager.getNetworkPortTemplates(context.valueOfFirst(), context.valueOfLast(),
+                context.valuesOfFilter(), context.valuesOfSelect());
+            out = results.getItems();
+        }
+        return out;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerAbstract#convertToResponse(org.ow2.sirocco.apis.rest.cimi.request.CimiContext,
+     *      java.lang.Object)
+     */
+    @Override
+    protected void convertToResponse(final CimiContext context, final Object dataService) throws Exception {
+        CimiNetworkPortTemplateCollectionRoot cimi = (CimiNetworkPortTemplateCollectionRoot) context.convertToCimi(dataService,
+            CimiNetworkPortTemplateCollectionRoot.class);
+        context.getResponse().setCimiData(cimi);
+        context.getResponse().setStatus(Response.Status.OK);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.ow2.sirocco.apis.rest.cimi.manager.CimiManagerAbstract#afterConvertToResponse(org.ow2.sirocco.apis.rest.cimi.request.CimiContext,
+     *      java.lang.Object)
+     */
+    @Override
+    protected void afterConvertToResponse(final CimiContext context, final Object dataService) {
+        super.afterConvertToResponse(context, dataService);
+        CimiResource resource = (CimiResource) context.getResponse().getCimiData();
+        resource.add(new CimiOperation(Operation.ADD.getRel(), resource.getId()));
+    }
+}
