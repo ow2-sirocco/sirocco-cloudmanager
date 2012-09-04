7ec98a425478391d9376c2eb7117afa9f7b4e6eb
Michel-Ange Anton <michel-ange.anton@bull.net>
2012-09-04 15:25:08 +0000

    Updated test with params

--- sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-apis/src/test/java/org/ow2/sirocco/apis/rest/cimi/manager/CimiManagersCredentialCollectionTest.java
+++ sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-apis/src/test/java/org/ow2/sirocco/apis/rest/cimi/manager/CimiManagersCredentialCollectionTest.java
@@ -25,6 +25,7 @@
 package org.ow2.sirocco.apis.rest.cimi.manager;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.easymock.EasyMock;
@@ -45,6 +46,7 @@ import org.ow2.sirocco.apis.rest.cimi.request.CimiSelect;
 import org.ow2.sirocco.apis.rest.cimi.request.RequestParams;
 import org.ow2.sirocco.apis.rest.cimi.utils.ConstantsPath;
 import org.ow2.sirocco.cloudmanager.core.api.ICredentialsManager;
+import org.ow2.sirocco.cloudmanager.core.api.QueryResult;
 import org.ow2.sirocco.cloudmanager.model.cimi.Credentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -126,60 +128,32 @@ public class CimiManagersCredentialCollectionTest {
         EasyMock.verify(this.service);
     }
 
-    // FIXME Adapt to the last CIMI specification
-    // @Test
-    // public void testReadWithCimiSelectAttributes() throws Exception {
-    //
-    // List<Credentials> list = new ArrayList<Credentials>();
-    // EasyMock.expect(
-    // this.service.getCredentials(EasyMock.eq(Arrays.asList(new String[]
-    // {"operations"})), EasyMock.eq((String) null)))
-    // .andReturn(list);
-    // EasyMock.replay(this.service);
-    //
-    // this.request.getHeader().getCimiSelect().setSelects(new String[]
-    // {"operations"});
-    // this.manager.execute(this.context);
-    //
-    // Assert.assertEquals(200, this.response.getStatus());
-    // Assert.assertEquals(ConstantsPath.CREDENTIAL_PATH,
-    // ((CimiCredentialsCollection) this.response.getCimiData()).getId());
-    //
-    // EasyMock.verify(this.service);
-    // }
+    @Test
+    public void testReadWithParams() throws Exception {
 
-    // FIXME Adapt to the last CIMI specification
-    // @Test
-    // public void testReadWithCimiSelectArrays() throws Exception {
-    // Credentials item;
-    // List<Credentials> list = new ArrayList<Credentials>();
-    // for (int i = 0; i < 23; i++) {
-    // item = new Credentials();
-    // item.setId(i + 13);
-    // list.add(item);
-    // }
-    //
-    // EasyMock.expect(
-    // this.service.getCredentials(EasyMock.eq(1), EasyMock.eq(23),
-    // EasyMock.eq(Arrays.asList(new String[]
-    // {"machineImages"})))).andReturn(list);
-    // EasyMock.replay(this.service);
-    //
-    // this.request.getHeader().getCimiSelect().setSelects(new String[]
-    // {"machineImages[1-23]"});
-    // this.manager.execute(this.context);
-    //
-    // Assert.assertEquals(200, this.response.getStatus());
-    // Assert.assertEquals(ConstantsPath.CREDENTIAL_PATH,
-    // ((CimiCredentialsCollection) this.response.getCimiData()).getId());
-    // CimiCredentialsCollection cimiCollect = (CimiCredentialsCollection)
-    // this.response.getCimiData();
-    // Assert.assertNotNull(cimiCollect.getArray());
-    // Assert.assertEquals(23, cimiCollect.getArray().length);
-    // for (int i = 0; i < cimiCollect.getArray().length; i++) {
-    // Assert.assertEquals(ConstantsPath.CREDENTIAL_PATH + "/" + (i + 13),
-    // cimiCollect.getArray()[i].getHref());
-    // }
-    // EasyMock.verify(this.service);
-    // }
+        List<Credentials> list = new ArrayList<Credentials>();
+        QueryResult<Credentials> result = new QueryResult<Credentials>(0, list);
+        EasyMock.expect(
+            this.service.getCredentials(EasyMock.eq(4), EasyMock.eq(99),
+                EasyMock.eq(Arrays.asList(new String[] {"filterOne"})),
+                EasyMock.eq(Arrays.asList(new String[] {"selectOne", "selectTwo"})))).andReturn(result);
+        EasyMock.replay(this.service);
+
+        this.request.getParams().setCimiFirst(new CimiIntegerParam("5"));
+        this.request.getParams().setCimiLast(new CimiIntegerParam("100"));
+        this.request.getParams().setCimiFilter(new CimiFilter(new String[] {"filterOne"}));
+        this.request.getParams().setCimiSelect(new CimiSelect(new String[] {"selectOne", "selectTwo"}));
+        this.manager.execute(this.context);
+
+        Assert.assertEquals(200, this.response.getStatus());
+        Assert.assertEquals(ConstantsPath.CREDENTIAL_PATH, ((CimiCredentialCollection) this.response.getCimiData()).getId());
+
+        EasyMock.verify(this.service);
+
+        // Reinit
+        this.request.getParams().setCimiFirst(new CimiIntegerParam());
+        this.request.getParams().setCimiLast(new CimiIntegerParam());
+        this.request.getParams().setCimiFilter(new CimiFilter());
+        this.request.getParams().setCimiSelect(new CimiSelect());
+    }
 }
