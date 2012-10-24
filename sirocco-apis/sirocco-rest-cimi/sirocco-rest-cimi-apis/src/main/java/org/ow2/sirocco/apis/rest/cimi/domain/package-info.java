717ec5f171fbcf0ce401b408180310c30d6b9272
Frederic Dang Tran <frederic.dangtran@orange.com>
2012-10-24 15:03:05 +0000

    Added package-level annotations to force the marshalling of an XML namespace attribute in CIMI resources

---
+++ sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-apis/src/main/java/org/ow2/sirocco/apis/rest/cimi/domain/package-info.java
@@ -0,0 +1,7 @@
+@XmlSchema(
+    namespace = "http://schemas.dmtf.org/cimi/1",
+    elementFormDefault = XmlNsForm.QUALIFIED)
+package org.ow2.sirocco.apis.rest.cimi.domain;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
