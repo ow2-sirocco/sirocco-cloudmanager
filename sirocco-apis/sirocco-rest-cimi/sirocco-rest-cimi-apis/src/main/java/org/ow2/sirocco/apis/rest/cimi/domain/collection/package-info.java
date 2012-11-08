713a25cc98a3c864135e02bff86685c33a924215
Frederic Dang Tran <frederic.dangtran@orange.com>
2012-11-08 11:03:21 +0000

    Delete module migrated to git

--- sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-apis/src/main/java/org/ow2/sirocco/apis/rest/cimi/domain/collection/package-info.java
+++
@@ -1,7 +0,0 @@
-@XmlSchema(
-    namespace = "http://schemas.dmtf.org/cimi/1",
-    elementFormDefault = XmlNsForm.QUALIFIED)
-package org.ow2.sirocco.apis.rest.cimi.domain.collection;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
--- sirocco-apis/sirocco-rest-cimi/sirocco-rest-cimi-webapp/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Use the specification "DMTF CIMI" to access Sirocco REST!</h2>
-</body>
-</html>
