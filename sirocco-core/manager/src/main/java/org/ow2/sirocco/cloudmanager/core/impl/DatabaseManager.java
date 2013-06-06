c285f49888b6414efcd010c6cec6a32188e14042
Cyril AUBOIN <cyril.auboin@auboin.com>
2013-06-06 17:33:11 +0200

    cleanup was working only for H2=> added support for mysql and choosing between 2 db thanks to entity manager property -not tested yet for H2-

--- sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/DatabaseManager.java
+++ sirocco-core/manager/src/main/java/org/ow2/sirocco/cloudmanager/core/impl/DatabaseManager.java
@@ -1,9 +1,12 @@
 package org.ow2.sirocco.cloudmanager.core.impl;
 
+import java.util.Map;
+
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 
 import org.ow2.sirocco.cloudmanager.core.api.IDatabaseManager;
@@ -17,54 +20,83 @@ import org.slf4j.LoggerFactory;
 public class DatabaseManager implements IDatabaseManager {
     private static Logger logger = LoggerFactory.getLogger(DatabaseManager.class.getName());
 
+    private static String JPA_IMPL = "eclipselink";
+    
     @PersistenceContext
     private EntityManager em;
+    
+    @Override
+    public String getUnderlyingDB() throws Exception{
+    	EntityManagerFactory emf = em.getEntityManagerFactory();     
+    	Map<String, Object> emfProperties = emf.getProperties();
+    	
+    	if ("eclipselink".equals(JPA_IMPL)){
+    		String driverClass = (String)emfProperties.get("eclipselink.target-database");
+    		return driverClass;
+    	}
+
+    	throw new Exception("JPA IMPLEMENTATION NOT SUPPORTED");
+    	
+    }
 
     @Override
-    public void cleanup() {
-        this.em.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE;").executeUpdate();
+    public void cleanup() throws Exception {
+    	
+    	
+    	String underlyingDB=getUnderlyingDB().toLowerCase();
+    	
+    	if (underlyingDB.contains("h2")){
+    		this.em.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE;").executeUpdate();		
+    	}else if (underlyingDB.contains("mysql")){
+    		this.em.createNativeQuery("SET foreign_key_checks = 0;").executeUpdate();	
+    	}
+    	
         DatabaseManager.logger.info("Cleaning up database");
         try {
-            this.em.createNativeQuery("DELETE FROM Job").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM JOB").executeUpdate();
 
-            this.em.createNativeQuery("DELETE FROM VolumeVolumeImage").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM VolumeConfiguration").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM VolumeImage").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM VolumeTemplate").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM Volume").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM VOLUMEVOLUMEIMAGE").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM VOLUMECONFIGURATION").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM VOLUMEIMAGE").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM VOLUMETEMPLATE").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM VOLUME").executeUpdate();
 
-            this.em.createNativeQuery("DELETE FROM CloudProviderLocation").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM CloudProviderAccount").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM CloudProvider").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM CLOUDPROVIDERLOCATION").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM CLOUDPROVIDERACCOUNT").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM CLOUDPROVIDER").executeUpdate();
 
-            this.em.createNativeQuery("DELETE FROM Machine").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM MachineConfiguration").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM MachineTemplate").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM MachineImage").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM MACHINE").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM MACHINECONFIGURATION").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM MACHINETEMPLATE").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM MACHINEIMAGE").executeUpdate();
 
-            this.em.createNativeQuery("DELETE FROM Credentials").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM CREDENTIALS").executeUpdate();
 
             this.em.createNativeQuery("DELETE FROM SYSTEMINSTANCE").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM SystemMachine").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM SystemVolume").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM SystemSystem").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM SystemTemplate").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM SYSTEMMACHINE").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM SYSTEMVOLUME").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM SYSTEMSYSTEM").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM SYSTEMTEMPLATE").executeUpdate();
 
-            this.em.createNativeQuery("DELETE FROM Credentials").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM CredentialsTemplate").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM CREDENTIALS").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM CREDENTIALSTEMPLATE").executeUpdate();
 
-            this.em.createNativeQuery("DELETE FROM Network").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM NetworkConfiguration").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM NetworkTemplate").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM NetworkPort").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM NetworkPortConfiguration").executeUpdate();
-            this.em.createNativeQuery("DELETE FROM NetworkPortTemplate").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM NETWORK").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM NETWORKCONFIGURATION").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM NETWORKTEMPLATE").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM NETWORKPORT").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM NETWORKPORTCONFIGURATION").executeUpdate();
+            this.em.createNativeQuery("DELETE FROM NETWORKPORTTEMPLATE").executeUpdate();
 
             this.em.createNativeQuery("DELETE FROM Users").executeUpdate();
         } catch (Exception e) {
             DatabaseManager.logger.error("Failed to delete some entities", e);
         } finally {
-            this.em.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE;").executeUpdate();
+        	if (underlyingDB.contains("h2")){
+        		this.em.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE;").executeUpdate();		
+        	}else if (underlyingDB.contains("mysql")){
+        		this.em.createNativeQuery("SET foreign_key_checks = 1;").executeUpdate();	
+        	}
         }
     }
 
