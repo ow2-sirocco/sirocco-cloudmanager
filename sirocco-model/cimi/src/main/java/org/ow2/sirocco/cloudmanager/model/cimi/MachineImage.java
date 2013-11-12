0470872245a4f63cf23ad0aee906833a4ee88dc0
Frederic Dang Tran <frederic.dangtran@orange.com>
2013-11-12 12:10:16 +0100

    Add UNKNOWN state and OS type, architecture attributes

--- sirocco-model/cimi/src/main/java/org/ow2/sirocco/cloudmanager/model/cimi/MachineImage.java
+++ sirocco-model/cimi/src/main/java/org/ow2/sirocco/cloudmanager/model/cimi/MachineImage.java
@@ -52,7 +52,7 @@ public class MachineImage extends CloudResource implements Serializable, IMultiC
     private CloudProviderLocation location;
 
     public static enum State {
-        CREATING, AVAILABLE, DELETING, ERROR, DELETED
+        CREATING, AVAILABLE, DELETING, ERROR, DELETED, UNKNOWN
     }
 
     public static enum Type {
@@ -72,6 +72,12 @@ public class MachineImage extends CloudResource implements Serializable, IMultiC
     private Visibility visibility = Visibility.PRIVATE;
 
     private List<ProviderMapping> providerMappings;
+
+    private Integer capacity;
+
+    private String architecture;
+
+    private String osType;
 
     @Enumerated(EnumType.STRING)
     public Visibility getVisibility() {
@@ -115,6 +121,30 @@ public class MachineImage extends CloudResource implements Serializable, IMultiC
 
     public void setImageLocation(final String imageLocation) {
         this.imageLocation = imageLocation;
+    }
+
+    public String getArchitecture() {
+        return this.architecture;
+    }
+
+    public void setArchitecture(final String architecture) {
+        this.architecture = architecture;
+    }
+
+    public String getOsType() {
+        return this.osType;
+    }
+
+    public void setOsType(final String osType) {
+        this.osType = osType;
+    }
+
+    public Integer getCapacity() {
+        return this.capacity;
+    }
+
+    public void setCapacity(final Integer capacity) {
+        this.capacity = capacity;
     }
 
     @ManyToOne
